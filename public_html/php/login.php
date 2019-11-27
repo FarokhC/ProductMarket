8ced68ef8d184e5f344d31eca3b14dc4ac7b0b44
Akash <akooshoo@yahoo.com>
2019-11-27 13:59:33 -0800

    Login checking, secure services section, user sessions (#3)
    * messing with login
    
    * login functionality

--- public_html/php/login.php
+++ public_html/php/login.php
@@ -1,13 +1,19 @@
 <?php
+    session_start();
+    if(isset($_SESSION['fail'])){
+      echo $_SESSION['fail'];
+      echo "<br>";
+    }
     echo "<h1>Secure Section Login</h1>";
 
     echo <<<END
-    <form method="post" action="/services.php">
+    <form method="post" action="/php/checkLogin.php">
       Username:<br>
-      <input type="text" name="username"><br>
+      <input type="text" name="user_name"><br>
       Password:<br>
       <input type="password" name="pass_word"><br><br>
       <input type="submit" value="submit" name = "submit">
     </form>
 END;
+    unset($_SESSION['fail']);
 ?>
